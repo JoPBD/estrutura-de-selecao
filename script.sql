65365b54a91296bb0e81a0a6060ad2bd8e5d211b
joabe oliveira <joabefatec01bd@outlook.com>
2023-09-12 09:08:36 -0300

    ap09(selecao): exemplo if/else

--- script.sql
+++ script.sql
@@ -1,15 +1,28 @@
 DO
 $$
 DECLARE 
-	valor INT;
+	valor INT := fn_valor_aleatorio_entre(1,100);
 BEGIN
-	valor := fn_valor_aleatorio_entre(1,10);
-	RAISE NOTICE 'O valor gerado fii %', valor;
-	IF valor < 20 THEN
-		RAISE NOTICE 'Sim, % é menor ou igual a 20',valor;
+	IF valor % 2 = 0 THEN
+		RAISE NOTICE '% é par',valor;
+	ELSE
+		RAISE NOTICE '% é impar1',valor;
 	END IF;
 END;
 $$
+
+-- DO
+-- $$
+-- DECLARE 
+-- 	valor INT;
+-- BEGIN
+-- 	valor := fn_valor_aleatorio_entre(1,10);
+-- 	RAISE NOTICE 'O valor gerado fii %', valor;
+-- 	IF valor < 20 THEN
+-- 		RAISE NOTICE 'Sim, % é menor ou igual a 20',valor;
+-- 	END IF;
+-- END;
+-- $$
 
 -- SELECT fn_valor_aleatorio_entre(2,5);
 
